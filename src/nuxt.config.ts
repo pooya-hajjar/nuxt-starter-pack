0d4766fd97339b04453bc87e1f9993cd4bf5089f
pooyaHajjjar <pooya.hhz@gmail.com>
2024-08-16 10:40:29 +0330

    chore: Add TailwindCSS module for styling

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -2,7 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: [
-    '@pinia/nuxt',
-  ],
+  modules: ['@pinia/nuxt', "@nuxtjs/tailwindcss"],
 })
