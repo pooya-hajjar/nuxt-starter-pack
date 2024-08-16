931d23c2305ec5c79406bd33c682e7dafe035df6
pooyaHajjjar <pooya.hhz@gmail.com>
2024-08-16 16:29:10 +0330

    feat: Add skeleton component

---
+++ src/components/ui/skeleton/u-skeleton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    role="status"
+    class="animate-pulse flex justify-center items-center bg-neutral-90 cursor-progress"
+    style="border-radius: 0.7rem; width: 100%; height: 100%"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
