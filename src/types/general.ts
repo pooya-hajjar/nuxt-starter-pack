04539bb7f20a781dba8ea25863da0ec5584d1e15
pooyaHajjjar <pooya.hhz@gmail.com>
2024-08-16 12:58:34 +0330

    feat: Add some prop enums and default color enum

---
+++ src/types/general.ts
@@ -0,0 +1,2 @@
+export type CPPBool = 0 | 1;
+export type Nullable<T> = T | null | undefined
--- src/types/propTypes.ts
+++ src/types/propTypes.ts
@@ -1,3 +1,52 @@
+import type { DefaultColors } from "./colors";
+
+enum Sizes {
+  sm,
+  md,
+  base,
+  lg,
+  xl,
+  "2xl",
+  "3xl",
+}
+
+enum BtnVariant {
+  "filled",
+  "elevated",
+  "text",
+  "outlined",
+}
+
+enum TextFieldVariant {
+  "outlined",
+  "underlined",
+  "flat",
+}
+
+enum TextFieldType {
+  "text",
+  "email",
+  "number",
+  "password",
+}
+
+enum LinkTarget {
+  "",
+  "_blank",
+  "_self",
+  "_parent",
+  "_top",
+}
+
+enum TextFieldInputMode {
+  "text",
+  "email",
+  "numeric",
+  "tel",
+  "decimal",
+  "url",
+}
+
 enum Cursors {
   "auto",
   "crosshair",
@@ -18,4 +67,25 @@ enum Cursors {
   "wait",
 }
 
+enum Currencies {
+  "toman",
+  "rial",
+}
+
+export type ColorsUnion = keyof typeof DefaultColors;
+export type SizesUnion = keyof typeof Sizes;
+export type RoundedUnion = SizesUnion | "none" | "full";
+export type StrokeUnion = "0" | "1" | "2" | "base" | "4" | "8" | "inset";
+export type WidthAndHeightUnion =
+  | number
+  | "auto"
+  | `${number}rem`
+  | `${number}px`
+  | `${number}%`;
+export type BtnVariantUnion = keyof typeof BtnVariant;
+export type TextFieldVariantUnion = keyof typeof TextFieldVariant;
+export type TextFieldTypeUnion = keyof typeof TextFieldType;
+export type LinkTargetUnion = keyof typeof LinkTarget;
+export type TextFieldModeUnion = keyof typeof TextFieldInputMode;
 export type CursorsUnion = keyof typeof Cursors;
+export type CurrencyUnion = keyof typeof Currencies;
