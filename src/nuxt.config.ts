7ae40d4f9a7c04bd1a895840e06edccd3978987b
pooyaHajjjar <pooya.hhz@gmail.com>
2024-08-16 12:14:14 +0330

    feat: Add SSR boolean to .env and configure usage

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: Boolean(process.env.SSR),
+
   compatibilityDate: "2024-04-03",
 
   runtimeConfig: {
